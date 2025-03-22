0e63482dc3bf6823d2cce3ba9e4e25dccb99d129
sho27ny <sho27nyo1@gmail.com>
2025-03-22 21:37:54 +0900

    不必要な記述の削除及び変更、曜日の再度表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -39,9 +39,8 @@ class CalendarsController < ApplicationController
       if wday_num >= 7             #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
-      wday_name = wdays[wday_num]
 
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wday_name}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays [wday_num]}
 
       @week_days.push(days)
     end
